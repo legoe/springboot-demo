7792d6b472f7f7a45c55ed0c2b02771b0244f00d
legoe <abtyl@126.com>
2019-12-03 12:01:42 +0800

    modify k8s method

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -12,16 +12,6 @@ public class K8SController {
 
     @RequestMapping("/k8s")
     public String k8s(){
-        try {
-            //用 getLocalHost() 方法创建的InetAddress的对象
-            InetAddress address = InetAddress.getLocalHost();
-            result="hostname: "+address.getHostName()+"hostaddress: "+address.getHostAddress();
-            System.out.println();//主机名
-            System.out.println();//主机别名
-            System.out.println();//
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        return "hello K8s <br/> "+result;
+        return "hello K8s <br/>111222 ";
     }
 }
